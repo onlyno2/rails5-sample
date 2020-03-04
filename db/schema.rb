b8844e8d31c576e71d37355a345b8c890d3b0488
kienn <kiennd010498@gmail.com>
2020-03-04 15:32:48 +0700

    user index delete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_095752) do
+ActiveRecord::Schema.define(version: 2020_03_04_081717) do
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -18,6 +19,8 @@ ActiveRecord::Schema.define(version: 2020_03_03_095752) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
 end
