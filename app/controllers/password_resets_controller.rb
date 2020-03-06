8afa59b695835ce12b3f5084ef25bb5d01ec7995
kienn <kiennd010498@gmail.com>
2020-03-06 14:10:45 +0700

    fix reset_password update

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -21,15 +21,15 @@ class PasswordResetsController < ApplicationController
   def edit; end
 
   def update
-    @user = User.find_by(email: params[:password_reset][:email].downcase)
-    if @user
-      @user.create_reset_digest
-      @user.send_password_reset_email
-      flash[:info] = 'Email sent with password reset instructions'
-      redirect_to root_url
+    if params[:user][:password].empty?
+      @user.errors.add(:password, "can't be empty")
+      render 'edit'
+    elsif @user.update_attributes(user_params)
+      log_in @user
+      flash[:success] = 'Password has been reset.'
+      redirect_to @user
     else
-      flash.now[:danger] = 'Email address not found'
-      render 'new'
+      render 'edit'
     end
   end
 
